793a5a617cb191f9b442eacebd1a6605d4da9ef1
Tyler Long <tyler4long@gmail.com>
2022-07-25 14:59:52 -0700

    Test '/restapi/v1.0/account/~/telephony/sessions'

---
+++ src/index3.ts
@@ -0,0 +1,23 @@
+import RingCentral from '@rc-ex/core';
+import PubNubExtension from '@rc-ex/pubnub';
+import waitFor from 'wait-for-async';
+
+const rc = new RingCentral({
+  server: 'https://platform.ringcentral.com',
+});
+
+const main = async () => {
+  rc.token = {access_token: process.env.TOKEN};
+  const pubNubExtension = new PubNubExtension();
+  await rc.installExtension(pubNubExtension);
+  await pubNubExtension.subscribe(
+    ['/restapi/v1.0/account/~/telephony/sessions'],
+    body => {
+      console.log(JSON.stringify(body, null, 2));
+    }
+  );
+
+  await waitFor({interval: 999999});
+};
+
+main();
